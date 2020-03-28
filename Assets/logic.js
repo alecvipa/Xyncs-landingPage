ab78aba6f60ad6406054fdadab54aaadc8da710f
alecvipa <hebrit_626@hotmail.com>
2020-03-28 15:04:48 -0600

    final front end version, missing mailer, responsiveness, scrollspy

--- Assets/logic.js
+++ Assets/logic.js
@@ -2,7 +2,19 @@
 $(document).ready(function () {
     var arrCounter = 0;
 
-
+    function arrow2(){
+        
+        $(".arrow2").hover(function(){
+            $(this).animate({ top: "+=3px" }, "normal");
+            $(this).animate({ top: "-=3px" }, "normal");
+            $(this).animate({ top: "+=3px" }, "normal");
+            $(this).animate({ top: "-=3px" }, "normal");
+            $(this).animate({ top: "+=3px" }, "normal");
+            $(this).animate({ top: "-=3px" }, "normal");
+                
+          });
+    }
+    arrow2();
     
     function arrow() {
         $(".arrow1").animate({ opacity: '0' });
@@ -14,20 +26,12 @@ $(document).ready(function () {
     };
     arrow();
 
-    // function btn2 () {
-    //     $(".descbtn2").animate({ opacity: '0.7' });
-    //     $(".descbtn2").animate({ opacity: '1' }, 1000);
-    //     btn2();
 
-    // };
     window.onscroll = function () {
         $(".intracont").animate({opacity: '1'},3000);
         counterBtn2 =0;
         $(".xyncsWhite").animate({ opacity: '1' }, 3000);
         $(".VR").animate({ opacity: '1' }, 3000);
-        // btn2();
-
-        
 
     };
 
